0666f006ee3961d77082237099f0443ace3b59d7
Simone407 <simonemazzeo703@gmail.com>
2025-02-17 10:56:04 +0100

    third commit

---
+++ es3.8.py
@@ -0,0 +1,9 @@
+places: list = ["londra","islanda","canarie","tokyo","amsterdam"] 
+
+sorted(places)
+
+print(places)
+
+sorted(places)
+
+print(places)
---
+++ es3.9.py
@@ -0,0 +1,14 @@
+people:list=["vannacci","feltri","elon musk","tonyeffe","gue","paolo"]
+
+print(f"mi dispiace{people[5]}ma non posso più ")
+people.pop(5)
+print(f"mi dispiace{people[1]}ma non posso più ")
+people.pop(1)
+print(f"mi dispiace{people[2]}ma non posso più ")
+people.pop(2)
+print(f"mi dispiace{people[1]}ma non posso più ")
+people.pop(1)
+
+print(people)
+
+len(people)
