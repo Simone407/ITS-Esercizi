01b7aeb450244fc1af05cd24fc0e3fcbc56133fa
Simone407 <simonemazzeo703@gmail.com>
2025-02-21 13:53:36 +0100

    6 commit

---
+++ Lezione_04/es4-1.py
@@ -0,0 +1,7 @@
+pizzas:list[str] = ["diavola","patate e salsiccia","margherita"]
+
+for pizzas in pizzas:
+    print(pizzas)
+    print(f"i like {pizzas} pizza")
+
+print("i really like pizza")
---
+++ Lezione_04/es4-2.py
@@ -0,0 +1,7 @@
+animal:list[str] = ["cat","dog","hamster"]
+
+for animal in animal:
+    print(animal)
+    print(f"a {animal} would make a great pet.")
+
+print("Any of these animals would make a great pet!")
