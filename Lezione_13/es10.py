31eabb0c958c297854fad5af1c064f65d61b3142
Simone407 <simonemazzeo703@gmail.com>
2025-04-03 11:24:02 +0200

    19 commit

---
+++ Lezione_13/es10.py
@@ -0,0 +1,13 @@
+'''
+
+Si scriva una funzione ricorsiva charDuplicator che consenta di duplicare ogni carattere in una stringa e restituisca il 
+risultato sotto forma di una nuova stringa.
+
+Ad esempio, se la stringa "libro" viene data in input a charDuplicator, la funzione ricorsiva deve produrre in
+ output la stringa "lliibbrroo".
+
+
+'''
+
+
+def charDuplicator() -> :
---
+++ Lezione_13/es2.py
@@ -0,0 +1,10 @@
+'''
+Si supponga di voler calcolare l'ammontare del denaro depositato su un conto bancario ad interesse composto. 
+Se m è la somma depositata sul conto, la somma disponibile alla fine del mese sarà 1.005 volte m.
+Scrivere una funzione ricorsiva compoundInterest che calcoli la somma presente sul conto dopo t mesi data una 
+somma di partenza m.
+
+'''
+
+def compundInterest() ->:
+    
