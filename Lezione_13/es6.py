309e9d57c2c858aac28650e24e8c4c0ebef2afb9
Simone407 <simonemazzeo703@gmail.com>
2025-04-03 11:24:02 +0200

    19 commit

---
+++ Lezione_13/es6.py
@@ -0,0 +1,10 @@
+'''
+
+Una produttoria è definita come il prodotto di un certo numero n di fattori, con n intero positivo. 
+Sia Pi una produttoria definita come segue:
+Pi = (0 + 2) * (1 + 2) * (2 + 2) * ... * (n + 2).  
+
+Calcolare il valore della produttoria Pi se n = 7.
+
+'''
+
