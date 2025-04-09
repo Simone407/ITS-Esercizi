46d620169f2a656ef26a02ea22c53e498d78a247
Simone407 <simonemazzeo703@gmail.com>
2025-04-09 19:23:12 +0200

    21 commit

---
+++ Lezione_14/es4.py
@@ -0,0 +1,6 @@
+'''
+Context Managers for File Handling: Use the with statement and context managers to open and close a file. 
+Handle potential IOError within the with block for clean resource management.
+'''
+
+
