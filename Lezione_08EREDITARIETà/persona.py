f9241bf6487bb9a73a078f5157c0edfd2eb58d9b
Simone407 <simonemazzeo703@gmail.com>
2025-04-15 13:18:32 +0200

    25 commit

---
+++ Lezione_08EREDITARIETà/persona.py
@@ -0,0 +1,47 @@
+
+class Persona:
+    def __init__(self, name: str, lastname: str, age: int):
+        self.setName(name)
+        self.setLastname(lastname)
+        self.setAge(age)
+
+    def __str__(self) -> str:
+
+        return f"\nNome: {self.name}\nCognome: {self.lastname}\nEtà: {self.age}"
+
+    def __call__(self):
+        if self.age < 18:
+            print(f"{self.name} {self.lastname} è minorenne")
+        elif 18 <= self.age < 30:
+            print(f"{self.name} {self.lastname} è maggiorenne!")
+        elif 30 <= self.age < 80:
+            print(f"{self.name} {self.lastname} è una persona adulta!")
+        else:
+            print(f"{self.name} {self.lastname} è una persona anziana!")
+
+    def setName(self, name: str) -> None:
+        if name:
+            self.name = name
+        else:
+            print("error")
+
+    def setLastname(self, lastname: str) -> None:
+        if lastname:
+            self.lastname = lastname
+        else:
+            print("error")
+
+    def setAge(self, age: int) -> None:
+        if age < 0 or age > 130:
+            self.age = 0
+        else:
+            self.age = age
+
+    def getName(self) -> str:
+        return self.name
+
+    def getLastname(self) -> str:
+        return self.lastname
+
+    def getAge(self) -> int:
+        return self.age
