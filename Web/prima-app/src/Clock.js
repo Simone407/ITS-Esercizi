d1dece49cc3163cafc04fdff3ed752c49291a498
Simone407 <simonemazzeo703@gmail.com>
2025-06-13 11:29:07 +0200

    46

---
+++ Web/prima-app/src/Clock.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Clock = (props) => {
+    const t = Date.now() + 3600*props.timezone*1000;
+    const date = new Date(t);
+  return (
+    <h3>in {props.country} sono le {date.toLocaleDateString()+ " " + date.toLocaleTimeString()}</h3>
+
+
+  )
+}
+
+export default Clock
---
+++ Web/prima-app/src/Componente1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Componente1 = (props) => {
+  return (
+    <div>Componente1{props.children}</div>
+  )
+}
+
+export default Componente1
