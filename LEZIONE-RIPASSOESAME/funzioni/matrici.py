0a310f79cc33e977ed1ff3f2cc061830642ebbae
Simone407 <simonemazzeo703@gmail.com>
2025-06-13 13:54:12 +0200

    47

--- LEZIONE-RIPASSOESAME/funzioni/matrici.py
+++ LEZIONE-RIPASSOESAME/funzioni/matrici.py
@@ -2,40 +2,26 @@
 def sum_primary_diagonal (matrix:list[list]) -> int:
 
     sum = 0
-    x,y = 0,0 
 
-    for (x,y) in matrix:
+    for i in range(len(matrix)):
 
-        sum = x[matrix(0)]
-
-        x +=1
-        y +=1
-
+        sum += matrix[i][i]
 
 
     return sum
 
 
-
-def sum_secondary_diagonal (matrix = ([9,8,7],[4,5,6],[1,2,3])) -> int:
+def sum_secondary_diagonal (matrix2 = ([9,8,7],[4,5,6],[1,2,3])) -> int:
     
-    sum = 0
-    x,y = 3,3 
+    sum2 = 0
 
-    for (x,y) in matrix:
+    for i in range(len(matrix2)):
 
-        sum = x[matrix(0)]
-
-        x +=1
-        y +=1
+        sum2 += matrix2[i][len(matrix2) -1-i]
 
 
-
-    return sum
-
+    return sum2
 
 
-
-
-sum_primary_diagonal([[1,2,3],[4,5,6],[7,8,9]]) 
-sum_secondary_diagonal([[1,2,3],[4,5,6],[7,8,9]]) 
+print(sum_primary_diagonal([[1,2,3],[4,5,6],[7,8,9]]))
+print(sum_secondary_diagonal([[1,2,3],[4,5,6],[7,8,9]]))
