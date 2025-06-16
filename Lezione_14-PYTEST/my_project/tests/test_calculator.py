b99872e8e0a5a0e23917d6c818a479702bc84372
Simone407 <simonemazzeo703@gmail.com>
2025-06-16 19:28:46 +0200

    48

---
+++ Lezione_14-PYTEST/my_project/tests/test_calculator.py
@@ -0,0 +1,21 @@
+from my_project import Calculator
+
+
+def test_addition():
+    calculation: Calculator = Calculator(10,5)
+    assert calculation.addition() == 13, 'the sum is wrong'
+
+
+def test_substraction():
+    calculation: Calculator = Calculator(10,5)
+    assert calculation.substraction() == 5, 'the substraction is wrong'
+
+
+def test_multiplication():
+    calculation: Calculator = Calculator(10,5)
+    assert calculation.multiplication() == 50, 'the sunm is wrong'
+
+
+def test_division():
+    calculation: Calculator = Calculator(10,5)
+    assert calculation.division() == 2.00, 'the quotient is wrong'
