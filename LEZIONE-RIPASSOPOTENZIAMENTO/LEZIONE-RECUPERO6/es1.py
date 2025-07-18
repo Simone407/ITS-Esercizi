53f68b5ddfdfe042fbd8820dcf6d6db633030a76
Simone407 <simonemazzeo703@gmail.com>
2025-07-18 17:00:40 +0200

    65

---
+++ LEZIONE-RIPASSOPOTENZIAMENTO/LEZIONE-RECUPERO6/es1.py
@@ -0,0 +1,59 @@
+'''
+Un palindromo è una stringa che non cambia anche se letta al contrario, come la stringa "radar". Si scriva una funzione ricorsiva chiamata recursivePalindrome() che accetti in input un parametro di tipo stringa e restituisca True se l'argomento è un palindromo e False altrimenti.
+
+Non si tenga conto degli spazi nella stringa e non si faccia distinzione tra lettere maiuscole e minuscole.
+
+La funzione dovrebbe considerare palindrome le seguenti stringhe:
+"radar"
+"Anna"
+" I topi non avevano nipoti"
+
+La funzione non dovrebbe considerare palindrome le seguenti stringhe:
+"casa"
+"Marta"
+"Roma e Amore"
+
+Suggerimento: usare la funzione replace() per sostituire gli spazi con una stringa vuota.
+'''
+
+
+
+
+
+
+
+def recursivePalindrome(a: str):
+
+    x: str = ""
+    is_palindrome = True
+
+    b = a.lower()
+    b = a.replace(" ",x)
+
+
+    if  b == a[::-1]:
+        return  is_palindrome
+
+    else:
+        is_palindrome = False
+
+    return is_palindrome
+
+print("ciao come va?: ", recursivePalindrome("ciao come va?"))
+
+print("----------------")
+
+print("radar: ",recursivePalindrome("radar"))
+print("Anna: ",recursivePalindrome("Anna"))
+print("i topi non avevano nipoti: ",recursivePalindrome("I topi non avevano nipoti"))
+
+print("----------------")
+
+print("Marta: ",recursivePalindrome("Marta"))
+print("Roma e Amore: ",recursivePalindrome("Roma e Amore"))
+print("Casa: ",recursivePalindrome("Casa"))
+
+
+
+
+
