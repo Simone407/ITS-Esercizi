e018068e40bf9d6df0d9a791e8f1a17484adbbb3
Simone407 <simonemazzeo703@gmail.com>
2025-11-27 13:58:00 +0100

    95

---
+++ JAVA/Lezione_01-PRIMIESERCIZI/terzo.java
@@ -0,0 +1,34 @@
+
+/* Esercizio n. 3
+Letto dalla tastiera un numero intero n > 0, stampare il fattoriale di n.
+Definizione di fattoriale: per n>1
+n! = n * ( n -1 ) * ( n -2 )... * 1.
+Per n= 1, n! = 1 */
+
+import java.util.Scanner;
+
+public class terzo {
+    
+    public static void main(String[] args) {
+        
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Inserisci un numero: ");
+        int n = s.nextInt();
+
+        int ris = 0;
+
+        for (int i = n; i>=1; i--){
+
+            ris += n * (n - (n-i));
+        }
+
+
+        System.out.println("il fattoriale di " + n + " è: " + ris);
+    }
+}
+
+
+// DA FINIRE
+
