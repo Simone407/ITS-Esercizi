8ef16bf5fcceea08bcfe9c593792bac59db1a213
Simone407 <simonemazzeo703@gmail.com>
2025-12-10 13:50:05 +0100

    104

--- Web/React-Native/Fullstack/App.js
+++ Web/React-Native/Fullstack/App.js
@@ -9,23 +9,44 @@ import {
   FlatList,
 } from "react-native";
 import TaskItem from "./components/TaskItem";
+import TaskInput from "./components/TaskInput";
 
 export default function App() {
-  const [task, setTask] = useState("");
+  // const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
-  function taskInputHandler(enteredTask) {
-    console.log(enteredTask);
-    setTask(enteredTask);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function startAddTask() {
+    setModalVisible(true); //Apre la modale
   }
 
-  function addTaskHandler() {
-    setTasks((current) => [...current, { task,id:new Date()}]);
-    setTask("");
+  function endAddTask() {
+    setModalVisible(false); //Apre la modale
   }
+
+  // function taskInputHandler(enteredTask) {
+  //   console.log(enteredTask);
+  //   setTask(enteredTask);
+  // }
+  function deleteTask(id) {
+    setTasks((current) => {
+      return current.filter((t) => t.id !== id);
+    });
+  }
+  function addTaskHandler(task) {
+    setTasks((current) => [...current, { task, id: new Date() }]);
+  }
+  // function addTaskHandler() {
+  //   setTasks((current) => [...current, { task,id:new Date()}]);
+  //   setTask("");
+  // }
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
+      <Button title="Add New Task" color="#5e0acc" onPress={startAddTask} />
+
+      <TaskInput visible={modalVisible} onAddTask={addTaskHandler} onCancel={endAddTask}></TaskInput>
+      {/* <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="Inserisci task"
@@ -37,20 +58,23 @@ export default function App() {
           onPress={addTaskHandler}
           disabled={task === ""}
         ></Button>
-      </View>
+      </View> */}
       <View style={styles.goalsContainer}>
-        <FlatList alwaysBounceVertical={true}
+        <FlatList
+          alwaysBounceVertical={true}
           data={tasks}
           renderItem={(itemData) => {
-           
             return (
-              <TaskItem taskItem={itemData.item}></TaskItem>
+              <TaskItem
+                onDelete={deleteTask}
+                taskItem={itemData.item}
+              ></TaskItem>
               // <View style={styles.taskItem}>
               //   <Text style={styles.taskText}>{itemData.item.task}</Text>
               // </View>
             );
           }}
-          keyExtractor={(item)=> item.id}
+          keyExtractor={(item) => item.id}
         />
         {/* <ScrollView>
           {tasks.map((t, index) => (
@@ -71,23 +95,22 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
   },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    padding: 8,
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderColor: "#cccccc",
-  },
+  // textInput: {
+  //   borderWidth: 1,
+  //   borderColor: "#cccccc",
+  //   width: "70%",
+  //   padding: 8,
+  // },
+  // inputContainer: {
+  //   flex: 1,
+  //   flexDirection: "row",
+  //   justifyContent: "space-between",
+  //   alignItems: "center",
+  //   marginBottom: 24,
+  //   borderBottomWidth: 1,
+  //   borderColor: "#cccccc",
+  // },
   goalsContainer: {
     flex: 4,
   },
-  
 });
