71bc75bff8472e349d235530053688e6935338e1
Simone407 <simonemazzeo703@gmail.com>
2026-01-14 19:12:41 +0100

    112

---
+++ PYTHON/Lezione_24-FLASK/voliaerei-flask-main/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+from db.utils import load_data_from_db
+
+app = Flask(__name__)
+
+@app.route('/')
+def initial_message():
+    return jsonify({"response":'Questo e il messaggio di benvenuto'})
+
+@app.route('/nazioni', methods=['GET'])
+def get_nazioni():
+    dati = load_data_from_db() # Carica TUTTI i dati dal finto database nel file json
+    nazioni: dict[str, dict[str, str]] = dati['Nazioni']
+    return jsonify(nazioni), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
